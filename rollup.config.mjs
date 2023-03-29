d27e4063faa34eab0d7f26771b2dccbba80c258d
JoostGevaert <jhngevaert@gmail.com>
2023-03-29 17:47:39 +0200

    refactor: project structure, index.html no bundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,16 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 
 export default [
   {
-    input: "./src/index.js",
-    output: [
-      {
-        format: "esm",
-        file: "./dist/bundle.js",
-      },
-    ],
-    plugins: [resolve()],
-  },
-  {
     input: "./src/wiv.js",
     output: [
       {
