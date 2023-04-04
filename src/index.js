1a26cc456654aac080214722b521d9b384dabb55
JoostGevaert <jhngevaert@gmail.com>
2023-04-05 00:57:58 +0200

    WIT upload

--- src/index.js
+++ src/index.js
@@ -2,36 +2,6 @@ import { createThreeScene } from "./three-scene";
 import { loadGltf } from "./gltf";
 import { uploadIfcWit } from "./wit";
 
-// Load a glTF in Three.js
-const gltfCanvas = document.getElementById("gltf");
-if (gltfCanvas) {
-  const [renderer, scene, clock, cameraControls] = createThreeScene(gltfCanvas);
-  const camera = scene.getObjectByName("camera");
-  cameraControls.setLookAt(15, 15, 15, 0, 10, 0);
-
-  gltfUrl = "../static/glTF/police_station.glb";
-  loaderContainer = document.getElementById("loader-container");
-  loadGltf(gltfUrl, scene, loaderContainer);
-
-  function animate() {
-    const delta = clock.getDelta();
-    cameraControls.update(delta);
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-  }
-
-  animate();
-}
-
-// Upload a glTF to a Three.js scene
-const gltfUploadCanvas = document.getElementById("upload-gltf");
-if (gltfUploadCanvas) {
-  const gltfUrl = "";
-  const loaderContainer = document.getElementById("loader-container");
-  if (!gltfUrl) {
-  }
-}
-
 // Upload an IFC file and visualize it with web-ifc-three (WIT)
 const witCanvas = document.getElementById("wit");
 if (witCanvas) {
